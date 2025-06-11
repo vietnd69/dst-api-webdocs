7d6b79b8621c1c50424e5a46fe0e954c34aee246
vietnd <vboy234@gmail.com>
2025-06-12 05:14:45 +0700

    Fix redirect path in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to="/dst-api-webdocs/getting-started" />;
+  return <Redirect to="/getting-started" />;
 } 
