37d796f65465e5d5dd3953d9b6f929eb32ac0081
vietnd <vboy234@gmail.com>
2025-06-12 04:42:52 +0700

    Fix redirect from homepage to getting-started

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,6 @@
-import React, { useEffect } from 'react';
-import { useHistory } from '@docusaurus/router';
+import React from 'react';
+import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
-  const history = useHistory();
-  
-  useEffect(() => {
-    history.replace('/getting-started');
-  }, [history]);
-  
-  return (
-    <div>
-      <p>Redirecting to Getting Started...</p>
-    </div>
-  );
+  return <Redirect to="/getting-started" />;
 } 
